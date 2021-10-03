8196f597b7634dbcd74c29dac36d8502d347d3b7
Andrzej J Skalski <gitstuff@s5i.ch>
2021-10-03 23:20:49 +0200

    quicksave

--- src/widget/list_widget.rs
+++ src/widget/list_widget.rs
@@ -1,3 +1,11 @@
+use crate::widget::widget::{Widget, WID, get_new_widget_id};
+use crate::primitives::xy::XY;
+use crate::io::input_event::InputEvent;
+use crate::widget::any_msg::AnyMsg;
+use crate::io::output::Output;
+use crate::io::style::{TextStyle_WhiteOnBlue, TextStyle_WhiteOnBlack, TextStyle_WhiteOnBrightYellow};
+use std::borrow::Borrow;
+
 pub enum ListWidgetCell {
     NotAvailable,
     Loading,
@@ -5,9 +13,121 @@ pub enum ListWidgetCell {
 }
 
 pub trait ListWidgetItem {
-    fn get_column_name(idx: usize) -> String;
     //TODO change to static str?
+    fn get_column_name(idx: usize) -> String;
+    fn get_min_column_width(idx: usize) -> u16;
     fn len_columns() -> usize;
     fn get(&self, idx: usize) -> ListWidgetCell;
+}
+
+impl<Item: ListWidgetItem> ListWidget<Item> {
+    pub fn new() -> Self {
+        ListWidget {
+            id: get_new_widget_id(),
+            items: vec![],
+            highlighted: None,
+            show_column_names: true,
+        }
+    }
+
+    pub fn with_items(self, items: Vec<Item>) -> Self {
+        ListWidget {
+            items,
+            ..self
+        }
+    }
+}
+
+pub struct ListWidget<Item: ListWidgetItem> {
+    id: WID,
+    // later probably change into some provider
+    items: Vec<Item>,
+    highlighted: Option<usize>,
+    show_column_names: bool,
+}
+
+impl<Item: ListWidgetItem> Widget for ListWidget<Item> {
+    fn id(&self) -> WID {
+        self.id
+    }
+
+    fn min_size(&self) -> XY {
+        // completely arbitrary
+
+        let rows = 2 + if self.show_column_names { 1 } else { 0 } as u16;
+        let mut cols = 0;
+
+        for i in 0..Item::len_columns() {
+            cols += Item::get_min_column_width(i);
+        }
+
+        XY::new(rows, cols)
+    }
+
+    fn size(&self, max_size: XY) -> XY {
+        self.min_size()
+    }
+
+    fn on_input(&self, input_event: InputEvent) -> Option<Box<dyn AnyMsg>> {
+        todo!()
+    }
+
+    fn update(&mut self, msg: Box<dyn AnyMsg>) -> Option<Box<dyn AnyMsg>> {
+        todo!()
+    }
+
+    fn get_focused(&self) -> &dyn Widget {
+        self
+    }
+
+    fn get_focused_mut(&mut self) -> &mut dyn Widget {
+        self
+    }
+
+    fn render(&self, focused: bool, output: &mut Output) {
+        // TODO add columns expansion
+        // it's the same as in layouts, probably we should move that calc to primitives
+        let mut y_offset: u16 = 0;
+
+        let header_style = TextStyle_WhiteOnBlue;
+        let non_highlighted_style = TextStyle_WhiteOnBlack;
+        let highlighted_style = TextStyle_WhiteOnBrightYellow;
+
+        if self.show_column_names {
+            let mut x_offset: u16 = 0;
+            for c_idx in 0..Item::len_columns() {
+                output.print_at(
+                    XY::new(x_offset, y_offset),
+                    header_style,
+                    Item::get_column_name(c_idx).as_str(), // TODO cut agaist overflow
+                );
+                x_offset += Item::get_min_column_width(c_idx);
+            }
+            y_offset += 1;
+        }
+
+        for (idx, item) in self.items.iter().enumerate() {
+            let style = if self.highlighted == Some(idx) {
+                highlighted_style
+            } else {
+                non_highlighted_style
+            };
+
+            for c_idx in 0..Item::len_columns() {
+                let text: String = match item.get(c_idx) {
+                    ListWidgetCell::NotAvailable => "N/A".to_string(),
+                    ListWidgetCell::Loading => "...".to_string(),
+                    ListWidgetCell::Ready(s) => s,
+                };
+
+                output.print_at(
+                    // TODO possible u16 overflow
+                    XY::new(x_offset + idx as u16, y_offset),
+                    style,
+                    text.as_str(),
+                );
+            }
+        }
+    }
 }
 
--- src/widget/mock_file_list.rs
+++ src/widget/mock_file_list.rs
@@ -8,6 +8,22 @@ struct MockFile {
     // there should be date but I'm in the plane and have no docs.
 }
 
+impl MockFile {
+    pub fn new(name: String, size: usize) -> Self {
+        MockFile {
+            name,
+            size,
+            filetype: None,
+        }
+    }
+
+    pub fn with_filetype(self, filetype: String) -> Self {
+        MockFile {
+            filetype: Some(filetype),
+            ..self
+        }
+    }
+}
 
 impl ListWidgetItem for MockFile {
     fn len_columns() -> usize {
@@ -31,12 +47,31 @@ impl ListWidgetItem for MockFile {
             _ => { ListWidgetCell::NotAvailable }
         }
     }
+
+    fn get_min_column_width(idx: usize) -> u16 {
+        match idx {
+            0 => 20,
+            1 => 12,
+            2 => 5,
+            _ => 0,
+        }
+    }
 }
 
 mod mock {
     use crate::widget::mock_file_list::MockFile;
 
     pub fn get_mock_file_list() -> Vec<MockFile> {
-        vec![]
+        let mut res: Vec<MockFile> = vec![];
+        for i in 0..10 {
+            res.push(
+                MockFile::new(format!("text_file_{}.txt", i), 1000 + i).with_filetype("txt".to_string())
+            );
+            res.push(
+                MockFile::new(format!("photo_{}.png", i), 30000 + i).with_filetype("png".to_string())
+            );
+        }
+
+        res
     }
 }
