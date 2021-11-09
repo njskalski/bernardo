2d2103defc1037e8b6280bd024bba05db2db3ba2
Andrzej J Skalski <gitstuff@s5i.ch>
2021-11-09 16:10:06 +0100

    quicksave: implementing cached_sizes in widgets 3

--- src/layout/cached_sizes.rs
+++ src/layout/cached_sizes.rs
@@ -1,3 +1,5 @@
+use crate::experiments::focus_group::{FocusGroup, FocusGroupImpl};
+use crate::experiments::from_geometry::from_wirs;
 use crate::layout::layout::{Layout, WidgetIdRect};
 use crate::primitives::xy::XY;
 use crate::widget::widget::Widget;
@@ -8,14 +10,17 @@ use crate::widget::widget::Widget;
 #[derive(Clone, Debug)]
 pub struct CachedSizes {
     pub for_size : XY,
-    pub widget_sizes : Vec<WidgetIdRect>
+    pub widget_sizes : Vec<WidgetIdRect>,
+    pub focus_group : Box<dyn FocusGroup>
 }
 
 impl CachedSizes {
     pub fn new(for_size : XY, widget_sizes : Vec<WidgetIdRect>) -> Self {
+        let focus_group = from_wirs(&widget_sizes, Some(for_size));
         CachedSizes {
             for_size,
             widget_sizes,
+            focus_group: Box::new(focus_group),
         }
     }
 }
