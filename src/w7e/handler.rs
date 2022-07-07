4de497f60a8846b3f6b859d5a66ba37dccac13f9
Andrzej J Skalski <gitstuff@s5i.ch>
2022-07-07 21:04:01 +0200

    qs

--- src/w7e/handler.rs
+++ src/w7e/handler.rs
@@ -2,14 +2,19 @@
 Handler is a wrapper that translates language specific project definition into common project
 items, like run configurations, test targets, and LSP clients.
  */
+use std::sync::Arc;
 use crate::fs::file_front::FileFront;
 use crate::LangId;
 use crate::w7e::handler_load_error::HandlerLoadError;
+use crate::w7e::navcomp_provider::NavCompProvider;
+
+pub type NavCompRef = Arc<Box<dyn NavCompProvider>>;
 
 pub trait Handler {
     fn lang_id(&self) -> LangId;
     fn handler_id(&self) -> &'static str;
-
     fn project_name(&self) -> &str;
+
+    fn navcomp(&self) -> Option<NavCompRef> { None }
 }
 
