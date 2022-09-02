71c03da6919ecb358e08e29c87f2ddabbc009951
Andrzej J Skalski <gitstuff@s5i.ch>
2022-09-02 23:52:19 +0200

    quicksave

--- src/gladius/logger_setup.rs
+++ src/gladius/logger_setup.rs
@@ -6,17 +6,21 @@ const DEBUG_PARAMS: &'static [(&'static str, log::LevelFilter)] = &[
     // I have no clue where it comes from, and I don't care so I suppress it
     ("mio::poll", log::LevelFilter::Error),
     // this is for "recursive_treat_views", which is the heart and backbone of Bernardo.
-    ("recursive_treat_views", log::LevelFilter::Debug),
+    ("recursive_treat_views", log::LevelFilter::Warn),
     ("bernardo::fs::local_filesystem_front", log::LevelFilter::Error),
     ("bernardo::text::buffer_state", log::LevelFilter::Warn),
     ("bernardo::tsw::tree_sitter_wrapper", log::LevelFilter::Error),
-    ("bernardo::widgets::main_view::main_view", log::LevelFilter::Debug),
+    ("bernardo::widgets::main_view::main_view", log::LevelFilter::Warn),
     ("bernardo::widgets::fuzzy_search::fuzzy_search", log::LevelFilter::Warn),
     ("bernardo::widgets::edit_box", log::LevelFilter::Warn),
 
     // This guy leaves a lot of data in trace, it seems like it spawns a new thread. I think it deserves profiling.
     ("arboard::x11_clipboard", log::LevelFilter::Warn),
-    ("bernardo::lsp_client", log::LevelFilter::Info),
+    ("bernardo::lsp_client", log::LevelFilter::Debug),
+    ("bernardo::w7e", log::LevelFilter::Debug),
+    ("bernardo::config", log::LevelFilter::Debug),
+    ("bernardo::lsp_client::lsp_read", log::LevelFilter::Warn),
+    ("bernardo::lsp_client::lsp_write", log::LevelFilter::Warn),
 ];
 
 
