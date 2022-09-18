aa6a3d43baed55ecf9ceebe49ce12eed9a646237
Andrzej J Skalski <gitstuff@s5i.ch>
2022-09-18 11:19:25 +0200

    dead end

--- src/widgets/spath_tree_view_node.rs
+++ src/widgets/spath_tree_view_node.rs
@@ -1,7 +1,12 @@
-use std::borrow::Cow;
+use std::borrow::{Borrow, Cow};
 use std::fmt::Debug;
+use std::marker::PhantomData;
+use std::ops::Deref;
+use std::sync::Arc;
 
+use jsonrpc_core::futures::stream::iter;
 use log::error;
+use owning_ref::OwningRef;
 
 use crate::fs::path::SPath;
 use crate::widgets::tree_view::tree_view_node::TreeViewNode;
@@ -36,6 +41,41 @@ impl FileTreeNode {
     }
 }
 
+// #[derive(Clone, Debug)]
+pub type NewFileTreeNode<const just_dirs: bool> = SPath;
+
+
+impl TreeViewNode<SPath> for NewFileTreeNode<true> {
+    fn id(&self) -> &SPath {
+        todo!()
+    }
+
+    fn label(&self) -> Cow<str> {
+        todo!()
+    }
+
+    fn is_leaf(&self) -> bool {
+        todo!()
+    }
+
+    fn child_iter(&self) -> Box<dyn Iterator<Item=&Self> + '_> {
+        match self.blocking_list().map(Deref::deref) {
+            Ok(items) => {
+                Box::new(items.iter())
+            }
+            Err(e) => {
+                error!("fail to call blocking_list {:?}", e);
+                Box::new(std::iter::empty()) as Box<dyn Iterator<Item=&Self>>
+            }
+        }
+    }
+
+
+    fn is_complete(&self) -> bool {
+        todo!()
+    }
+}
+
 impl TreeViewNode<SPath> for FileTreeNode {
     fn id(&self) -> &SPath {
         &self.sp
@@ -50,15 +90,14 @@ impl TreeViewNode<SPath> for FileTreeNode {
     }
 
     fn child_iter(&self) -> Box<dyn Iterator<Item=&Self> + '_> {
-        match self.sp.blocking_list() {
-            Ok(items) => Box::new(
-                items.iter().map(|item| &FileTreeNode::new(item))
-            ) as Box<dyn Iterator<Item=Self>>,
-            Err(e) => {
-                error!("fail to call blocking_list {:?}", e);
-                Box::new(std::iter::empty()) as Box<dyn Iterator<Item=Self>>
-            }
-        }
+        todo!()
+        // match self.sp.blocking_list_2() {
+        //     Ok(items) => items,
+        //     Err(e) => {
+        //         error!("fail to call blocking_list {:?}", e);
+        //         Box::new(std::iter::empty()) as Box<dyn Iterator<Item=&Self>>
+        //     }
+        // }
     }
 
     fn is_complete(&self) -> bool {
@@ -78,15 +117,16 @@ impl TreeViewNode<SPath> for DirTreeNode {
     }
 
     fn child_iter(&self) -> Box<dyn Iterator<Item=&Self> + '_> {
-        match self.sp.blocking_list() {
-            Ok(items) => Box::new(
-                items.into_iter().filter(|c| c.is_dir()).map(|item| DirTreeNode::new(item))
-            ) as Box<dyn Iterator<Item=Self>>,
-            Err(e) => {
-                error!("fail to call blocking_list {:?}", e);
-                Box::new(std::iter::empty()) as Box<dyn Iterator<Item=Self>>
-            }
-        }
+        todo!()
+        // match self.sp.blocking_list() {
+        //     Ok(items) => Box::new(
+        //         items.into_iter().filter(|c| c.is_dir()).map(|item| &DirTreeNode::new(item))
+        //     ) as Box<dyn Iterator<Item=&Self>>,
+        //     Err(e) => {
+        //         error!("fail to call blocking_list {:?}", e);
+        //         Box::new(std::iter::empty()) as Box<dyn Iterator<Item=&Self>>
+        //     }
+        // }
     }
 
     fn is_complete(&self) -> bool {
