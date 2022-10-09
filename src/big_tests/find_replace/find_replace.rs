79548fb121979064f5e741b3bb278fae52f9dbe5
Andrzej J Skalski <gitstuff@s5i.ch>
2022-10-09 21:56:32 +0200

    find-replace tests

--- src/big_tests/find_replace/find_replace.rs
+++ src/big_tests/find_replace/find_replace.rs
@@ -1,3 +1,4 @@
+use crate::io::keys::Keycode;
 use crate::mocks::full_setup::FullSetup;
 
 fn common_start() -> FullSetup {
@@ -30,6 +31,15 @@ fn find_is_focused() {
 }
 
 #[test]
+fn esc_closes_find() {
+    let mut full_setup = common_start();
+    full_setup.send_key(Keycode::Esc.to_key());
+    assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().find_op().is_none()));
+    assert!(full_setup.get_first_editor().unwrap().is_view_focused());
+    full_setup.finish();
+}
+
+#[test]
 fn opening_replace_moves_focus() {
     let mut full_setup = common_start();
     assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().find_op().unwrap().is_focused()));
@@ -40,6 +50,7 @@ fn opening_replace_moves_focus() {
 
     assert!(full_setup.get_first_editor().unwrap().replace_op().unwrap().is_focused());
     assert_eq!(full_setup.get_first_editor().unwrap().find_op().unwrap().is_focused(), false);
+    full_setup.finish();
 }
 
 #[test]
@@ -56,4 +67,20 @@ fn opening_replace_retains_contents_of_find() {
     assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().replace_op().is_some()));
 
     assert!(full_setup.get_first_editor().unwrap().find_op().unwrap().contents().contains("twoja stara"));
+    full_setup.finish();
+}
+
+#[test]
+fn esc_closes_both() {
+    let mut full_setup = common_start();
+    assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().find_op().unwrap().is_focused()));
+
+    full_setup.send_key(full_setup.config().keyboard_config.editor.replace);
+
+    assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().replace_op().is_some()));
+
+    full_setup.send_key(Keycode::Esc.to_key());
+    assert!(full_setup.wait_for(|f| f.get_first_editor().unwrap().find_op().is_none()));
+    assert!(full_setup.get_first_editor().unwrap().is_view_focused());
+    full_setup.finish();
 }
