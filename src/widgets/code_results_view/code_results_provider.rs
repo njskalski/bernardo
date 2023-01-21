731e0433833d24139f68400a832abc3c598b94cc
Andrzej J Skalski <gitstuff@s5i.ch>
2023-01-21 13:03:20 +0100

    qs

--- src/widgets/code_results_view/code_results_provider.rs
+++ src/widgets/code_results_view/code_results_provider.rs
@@ -1,7 +1,11 @@
 use crate::io::loading_state::LoadingState;
+use crate::w7e::navcomp_provider::SymbolUsage;
 
+/*
+Invariant: if items becomes longer, the initial items DO NOT CHANGE.
+ */
 pub trait CodeResultsProvider {
     fn loading_state(&self) -> LoadingState;
 
-    // fn items(&self) -> Box<dyn Iterator<Item=>>
+    fn items(&self) -> Box<dyn Iterator<Item=SymbolUsage> + '_>;
 }
--- src/widgets/code_results_view/promise_provider.rs
+++ src/widgets/code_results_view/promise_provider.rs
@@ -1,6 +1,10 @@
+use std::iter;
+
+use log::debug;
+
 use crate::io::loading_state::LoadingState;
 use crate::promise::promise::PromiseState;
-use crate::w7e::navcomp_provider::SymbolUsagesPromise;
+use crate::w7e::navcomp_provider::{SymbolUsage, SymbolUsagesPromise};
 use crate::widgets::code_results_view::code_results_provider::CodeResultsProvider;
 
 impl CodeResultsProvider for SymbolUsagesPromise {
@@ -9,6 +13,19 @@ impl CodeResultsProvider for SymbolUsagesPromise {
             PromiseState::Unresolved => LoadingState::InProgress,
             PromiseState::Ready => LoadingState::Complete,
             PromiseState::Broken => LoadingState::Error,
+        }
+    }
+
+    // TODO this entire method is a stub. It should not copy, it should stream and stuff.
+    fn items(&self) -> Box<dyn Iterator<Item=SymbolUsage> + '_> {
+        match self.read() {
+            None => {
+                Box::new(iter::empty()) as Box<dyn Iterator<Item=SymbolUsage>>
+            }
+            Some(vec) => {
+                debug!("returning {} results", vec.len());
+                Box::new(vec.iter().map(|c| c.clone()))
+            }
         }
     }
 }
