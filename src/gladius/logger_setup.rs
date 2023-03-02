b6fd4f593ffdc364d1b503c83a0352645b401a66
Andrzej J Skalski <gitstuff@s5i.ch>
2023-03-02 16:58:01 +0100

    denoise

--- src/gladius/logger_setup.rs
+++ src/gladius/logger_setup.rs
@@ -3,21 +3,23 @@ use log::{LevelFilter, warn};
 const DEBUG_PARAMS: &'static [(&'static str, log::LevelFilter)] = &[
     // this is for git ignore
     ("globset", log::LevelFilter::Error),
+
     // I have no clue where it comes from, and I don't care so I suppress it
     ("mio::poll", log::LevelFilter::Error),
     // this is for "recursive_treat_views", which is the heart and backbone of Bernardo.
     ("recursive_treat_views", log::LevelFilter::Info),
+    ("bernardo", log::LevelFilter::Debug),
+    ("bernardo::primitives::tmtheme", log::LevelFilter::Info),
     ("bernardo::fs::local_filesystem_front", log::LevelFilter::Error),
     ("bernardo::gladius::run_gladius", log::LevelFilter::Info),
     ("bernardo::text::buffer_state", log::LevelFilter::Warn),
-    ("bernardo::tsw::tree_sitter_wrapper", log::LevelFilter::Debug),
-    ("bernardo", log::LevelFilter::Debug),
+    ("bernardo::tsw::tree_sitter_wrapper", log::LevelFilter::Error),
     ("bernardo::widgets::completion_widget", log::LevelFilter::Debug),
     ("bernardo::widgets::dir_tree_view", log::LevelFilter::Warn),
     ("bernardo::widgets::edit_box", log::LevelFilter::Warn),
     ("bernardo::widgets::fuzzy_search::fuzzy_search", log::LevelFilter::Warn),
     ("bernardo::widgets::list_widget::list_widget", log::LevelFilter::Warn),
-    ("bernardo::widgets::main_view::main_view", log::LevelFilter::Debug),
+    ("bernardo::widgets::main_view::main_view", log::LevelFilter::Info),
     ("bernardo::widgets::save_file_dialog::save_file_dialog", log::LevelFilter::Warn),
     ("bernardo::layout", log::LevelFilter::Info),
     // ("bernardo::layout::leaf_layout", log::LevelFilter::Debug),
