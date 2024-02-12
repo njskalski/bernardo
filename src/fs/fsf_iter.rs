25cc22212c15a3ac943afd0624c89d5a9dfc6673
Gokul Soumya <gokulps15@gmail.com>
2024-02-13 00:51:46 +0530

    Split tests for nested ignore files

--- src/fs/fsf_iter.rs
+++ src/fs/fsf_iter.rs
@@ -155,7 +155,6 @@ mod tests {
             .with_file("folder1/folder2/.gitignore", "file1.txt")
             .with_file("folder1/folder2/file1.txt", "not matched")
             .with_file("folder1/folder3/moulder.txt", "truth is out there")
-            .with_file("folder1/folder3/file1.txt", "matched")
             .to_fsf();
 
         let mut iter = RecursiveFsIter::new(m.root());
@@ -164,6 +163,23 @@ mod tests {
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2").unwrap()));
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3").unwrap()));
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3", "moulder.txt").unwrap()));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_nested_gitignore_does_not_affect_other_files() {
+        let m = MockFS::new("/tmp")
+            .with_file("folder1/folder2/.gitignore", "file1.txt")
+            .with_file("folder1/folder2/file1.txt", "not matched")
+            .with_file("folder1/folder3/file1.txt", "matched")
+            .to_fsf();
+
+        let mut iter = RecursiveFsIter::new(m.root());
+
+        assert_eq!(iter.next(), Some(spath!(m, "folder1").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3").unwrap()));
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3", "file1.txt").unwrap()));
         assert_eq!(iter.next(), None);
     }
