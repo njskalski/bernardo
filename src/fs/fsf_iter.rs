dd381859308f16b7aaef32c99f3665721a296499
Gokul Soumya <gokulps15@gmail.com>
2024-02-12 20:54:11 +0530

    Add failing tests for respecting gitignores

--- src/fs/fsf_iter.rs
+++ src/fs/fsf_iter.rs
@@ -34,6 +34,7 @@ impl Iterator for RecursiveFsIter {
     fn next(&mut self) -> Option<Self::Item> {
         while let Some(iter) = self.stack.last_mut() {
             let Some(item) = iter.next() else {
+                // The iter is empty; remove it
                 self.stack.pop();
                 continue;
             };
@@ -95,6 +96,44 @@ mod tests {
         assert_eq!(iter.next(), Some(spath!(m, "folder1").unwrap()));
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2").unwrap()));
         assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2", "file1.txt").unwrap()));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_root_gitignore_is_respected() {
+        let m = MockFS::new("/tmp")
+            .with_file(".gitignore", "file1.txt")
+            .with_file("folder1/folder2/file1.txt", "not matched")
+            .with_file("folder1/folder3/moulder.txt", "truth is out there")
+            .to_fsf();
+
+        let mut iter = RecursiveFsIter::new(m.root());
+
+        assert_eq!(iter.next(), Some(spath!(m, "folder1").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3", "moulder.txt").unwrap()));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_nested_gitignore_is_respected() {
+        let m = MockFS::new("/tmp")
+            .with_file("folder1/folder2/.gitignore", "file1.txt")
+            .with_file("folder1/folder2/file1.txt", "not matched")
+            .with_file("folder1/folder3/moulder.txt", "truth is out there")
+            .with_file("folder1/folder3/file1.txt", "matched")
+            .to_fsf();
+
+        let mut iter = RecursiveFsIter::new(m.root());
+
+        assert_eq!(iter.next(), Some(spath!(m, "folder1").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder2").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3", "moulder.txt").unwrap()));
+        assert_eq!(iter.next(), Some(spath!(m, "folder1", "folder3", "file1.txt").unwrap()));
         assert_eq!(iter.next(), None);
     }
 }
