2b2f32ea0af865a27dc01b18d0050d5065ffa0da
Andrzej J Skalski <gitstuff@s5i.ch>
2024-03-31 21:10:11 +0200

    context tree

--- src/widgets/editor_widget/context_bar/context_bar_item.rs
+++ src/widgets/editor_widget/context_bar/context_bar_item.rs
@@ -1,102 +1,79 @@
+use crate::primitives::tree::tree_node::TreeNode;
 use std::borrow::Cow;
 use std::fmt::Debug;
 use std::rc::Rc;
+use std::sync::Arc;
 
 use crate::widget::any_msg::{AnyMsg, AsAny};
 use crate::widgets::editor_widget::msg::EditorWidgetMsg;
 use crate::widgets::list_widget::list_widget_item::ListWidgetItem;
 
-// I think I want the "context bar" to be "cascading", enabling a "conversational like" interface.
-// Because some of commands will yield "options" to choose from, say "usages" will show up multiple
-// options. Here's how I imagine that.
-
-/*
-   box drawing symbols are for clarity only, I will use colors to distinguish between layers and
-   save screen space
-
-   somesymbol
-       ┌──────────────────── ...
-       │ go to definition
-       │ [find usages]
-       │ some other option
-       │
-      ...
-
-    hit enter
-   somesymbol
-       ┌──────────────────── ...
-       │ find usages ↴
-       │ ┌────────────────── ...
-       │ │ usage 1 (file X)
-       │ │ usage 2 (file Y)
-       │ │
-      .....
-
-   escape goes back
-
-   findings:
-   1) Would be great to anchor the bar in the middle of symbol (say middle of word).
-      At the same time, depending on language, different symbols can be even queried. For instance,
-      one cannot overload operators in pure C (so there is no good definition for == operator)
-      but in say Scala, pretty much everything like "-++-" can become an operator and "jump to
-      definition" of it makes perfect sense.
-      So "what even is the symbol under my cursor" is a call to NavComp provider.
-
-*/
-
 pub type Action = fn() -> Box<dyn AnyMsg>;
+pub type Key = Cow<'static, str>;
 
-/*
-TODO
- I am not sure how this struct should look like inside, I just know how I want it to look in UI.
- Therefore I delay this design until I have working tests.
- */
+#[derive(Debug, Clone)]
+pub enum NodeType {
+    Leaf(Action),
+    InternalNode(Vec<ContextBarItem>),
+}
+
 #[derive(Debug, Clone)]
 pub struct ContextBarItem {
     title: Cow<'static, str>,
-    action: Action,
+    node_type: NodeType,
 }
 
 impl ContextBarItem {
+    pub fn new_internal_node(title: Cow<'static, str>, children: Vec<ContextBarItem>) -> ContextBarItem {
+        ContextBarItem {
+            title,
+            node_type: NodeType::InternalNode(children),
+        }
+    }
+
     pub const GO_TO_DEFINITION: ContextBarItem = ContextBarItem {
         title: Cow::Borrowed("go to definition"),
-        action: || EditorWidgetMsg::GoToDefinition.boxed(),
+        node_type: NodeType::Leaf(|| EditorWidgetMsg::GoToDefinition.boxed()),
     };
     pub const REFORMAT_FILE: ContextBarItem = ContextBarItem {
         title: Cow::Borrowed("reformat file"),
-        action: || EditorWidgetMsg::Reformat.boxed(),
+        node_type: NodeType::Leaf(|| EditorWidgetMsg::Reformat.boxed()),
     };
     pub const SHOW_USAGES: ContextBarItem = ContextBarItem {
         title: Cow::Borrowed("show usages"),
-        action: || EditorWidgetMsg::ShowUsages.boxed(),
+        node_type: NodeType::Leaf(|| EditorWidgetMsg::ShowUsages.boxed()),
     };
     // TODO add reformat selection
-
-    pub fn msg(&self) -> Box<dyn AnyMsg> {
-        (self.action)()
-    }
+    //
+    // pub fn msg(&self) -> Box<dyn AnyMsg> {
+    //     (self.action)()
+    // }
 }
 
-impl ListWidgetItem for ContextBarItem {
-    fn get_column_name(_idx: usize) -> &'static str {
-        "name"
+impl TreeNode<Key> for ContextBarItem {
+    fn id(&self) -> &Key {
+        &self.title
     }
 
-    fn get_min_column_width(idx: usize) -> u16 {
-        match idx {
-            0 => 10,
-            _ => 0,
+    fn label(&self) -> Cow<str> {
+        self.title.clone()
+    }
+
+    fn is_leaf(&self) -> bool {
+        match self.node_type {
+            NodeType::Leaf(_) => true,
+            NodeType::InternalNode(_) => false,
         }
     }
 
-    fn len_columns() -> usize {
-        1
+    fn child_iter(&self) -> Box<dyn Iterator<Item = Self>> {
+        match &self.node_type {
+            NodeType::Leaf(_) => Box::new(std::iter::Empty::default()),
+            NodeType::InternalNode(items) => Box::new(items.clone().into_iter()),
+        }
     }
 
-    fn get(&self, idx: usize) -> Option<Rc<String>> {
-        match idx {
-            0 => Some(Rc::new(self.title.to_string())),
-            _ => None,
-        }
+    fn is_complete(&self) -> bool {
+        true
     }
 }
