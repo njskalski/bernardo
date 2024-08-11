01eb784ac0839fe8a7048458d2123b859ea432c3
Andrzej J Skalski <gitstuff@s5i.ch>
2024-08-11 22:50:47 +0200

    nix tmp

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
+
+let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "foo-bar";
-  version = "0.1";
+  pname = manifest.name;
+  version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
+  checkType = "debug";
+  cargoParallelTestThreads = false;
+  fetchSubmodules = true;
 }
