c4cc3a0d645e484dbf2149df51ac3b2403e07d6a
Grzegorz Dolanowski <grzegorz.dolanowski@gmail.com>
2025-01-15 00:05:29 +0100

    fmt

--- src/big_tests/context_kite_and_scroll/context_kite_and_scroll_test_1.rs
+++ src/big_tests/context_kite_and_scroll/context_kite_and_scroll_test_1.rs
@@ -70,33 +70,35 @@ fn context_kite_and_scroll_test_1() {
 
 #[test]
 fn context_kite_and_scroll_test_2() {
-        let mut full_setup: FullSetup = FullSetup::new("./test_envs/context_kite_and_scroll_1")
-            .with_files(["long_line.txt"])
-            .build();
+    //THIS TEST SHOLD BE RUN IN RELEASE
+    //IN DEBUG IT CRASHES ON DEBUG_ASSERTS IN SCROLL
+    let mut full_setup: FullSetup = FullSetup::new("./test_envs/context_kite_and_scroll_1")
+        .with_files(["long_line.txt"])
+        .build();
 
-        assert!(full_setup.wait_for(|f| f.is_editor_opened()));
-        assert_eq!(
-            full_setup
-                .get_first_editor()
-                .unwrap()
-                .get_visible_cursor_line_indices()
-                .map(|c| c.visible_idx)
-                .collect::<Vec<usize>>(),
-            vec![1]
-        );
+    assert!(full_setup.wait_for(|f| f.is_editor_opened()));
+    assert_eq!(
+        full_setup
+            .get_first_editor()
+            .unwrap()
+            .get_visible_cursor_line_indices()
+            .map(|c| c.visible_idx)
+            .collect::<Vec<usize>>(),
+        vec![1]
+    );
 
-        assert!(full_setup.send_key(Keycode::ArrowRight.to_key().with_ctrl())); //go to the end of long line
-        assert!(full_setup.send_key(Keycode::ArrowDown.to_key())); //go to end of short line
-        full_setup.wait_for(|full_setup| {
-            full_setup
-                .get_first_editor()
-                .unwrap()
-                .get_visible_cursor_cells()
-                .next()
-                .map(|item| item.1.grapheme() == Some(""))
-                .unwrap_or(false)
-        });
+    assert!(full_setup.send_key(Keycode::ArrowRight.to_key().with_ctrl())); //go to the end of long line
+    assert!(full_setup.send_key(Keycode::ArrowDown.to_key())); //go to end of short line
+    full_setup.wait_for(|full_setup| {
+        full_setup
+            .get_first_editor()
+            .unwrap()
+            .get_visible_cursor_cells()
+            .next()
+            .map(|item| item.1.grapheme() == Some(""))
+            .unwrap_or(false)
+    });
 
-        full_setup.screenshot();
-        assert!(full_setup.get_first_editor().unwrap().get_visible_cursor_cells().next().is_some());
+    full_setup.screenshot();
+    assert!(full_setup.get_first_editor().unwrap().get_visible_cursor_cells().next().is_some());
 }
