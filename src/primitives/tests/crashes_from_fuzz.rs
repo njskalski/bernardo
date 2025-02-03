7a99e7b4ccc05dad63e6bb8d2babf16b1dbc43be
Andrzej J Skalski <gitstuff@s5i.ch>
2025-02-03 09:12:20 +0100

    second crash

--- src/primitives/tests/crashes_from_fuzz.rs
+++ src/primitives/tests/crashes_from_fuzz.rs
@@ -19,3 +19,20 @@ fn crash_1() {
 
     fuzz_call(text, msgs);
 }
+
+#[test]
+fn crash_2() {
+    let text = "}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{11}\u{18}\u{18}\u{18}\u{18}\u{18}\rzzw".to_string();
+    let msgs: Vec<CommonEditMsg> = vec![
+        Block("\u{11}\u{11}\u{11}\u{11}\u{11}\0\0\u{1b}`\0\u{8}".to_string()),
+        Char('\u{114c2}'),
+        Block("\u{11}\u{11}\u{11}\u{18}\u{18}\u{18}\u{18}\u{18}".to_string()),
+        WordEnd { selecting: true },
+        SubstituteBlock {
+            char_range: 0..46,
+            with_what: "".to_string(),
+        },
+    ];
+
+    fuzz_call(text, msgs);
+}
